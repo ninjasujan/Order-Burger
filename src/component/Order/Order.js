3fd0340c013997216f74c81f4b4722377288a91d
sujan <sujanpoojary884@gmail.com>
2020-06-14 11:22:12 +0530

    old orders fetched and set order routes

--- src/component/Order/Order.js
+++ src/component/Order/Order.js
@@ -3,16 +3,33 @@
 import React from 'react';
 import classes from './Order.css';
 
-const Order = (props) => (
-  <div className={classes.Order}>
-    <h3>Ingredients</h3>
-    <p>Salad: 1</p>
-    <p>Bacon: 1</p>
-    <p>Cheese: 1</p>
-    <p>Meat: 1</p>
-    <h3>Price: 5</h3>
-  </div>
-);
+const Order = (props) => {
+  const ingredients = [];
+  for (let ingName in props.ingredients) {
+    ingredients.push({
+      name: ingName,
+      qty: props.ingredients[ingName],
+    });
+  }
+
+
+  const ingredientsOutput = ingredients.map(ing => {
+    return <span style={{
+      textTransform: 'capitalize',
+      boxShadow: '0 2px 3px #ccc',
+      margin: '3px',
+      padding: '4px'
+    }} key={ing.name}>{ing.name}: ({ing.qty}) </span>
+  });
+
+  return (
+    <div className={classes.Order}>
+      <h3>Ingredients</h3>
+      {ingredientsOutput}
+      <h3>Price: ${Number.parseFloat(props.price.toFixed(2))}</h3>
+    </div>
+  );
+};
 
 
 export default Order;
